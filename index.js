7a7dab6089454b454c8f17d91264a1fac4ae690a
Joe Alves <jmalves@appacademy.io>
2021-04-18 16:01:06 -0400

    ticking class

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+
+// be instantiated with a BPM
+// allow for reassigning the BPM
+// control the interval of the ticks
+// and broadcast to registered listeners
+class Ticking {
+    constructor(bpm, start = false) {
+        this.currentTicking = null;
+        this.bpm = bpm;
+        this.tickListeners = [];
+        if (start) {
+            this.startPlaying();
+        }
+    }
+    startPlaying() {
+        this.currentTicking = setInterval(() => {
+            this.tickListeners.forEach(l => l());
+        }, Ticking.bpmToMilliseconds(this.bpm));
+    }
+    stopPlaying() {
+        clearInterval(this.currentTicking);
+        this.currentTicking = null;
+    }
+    onTick(listener) {
+        this.tickListeners.push(listener);
+    }
+    static bpmToMilliseconds(bpmValue) {
+        // 60bpm -> 1 beat every second 
+        // 30bpm -> 1 beat every 2 seconds
+        // 120bpm -> 2 beats every second
+        const amountOfMillisecondsInAMinute = 1000 * 60;
+        return amountOfMillisecondsInAMinute / bpmValue;
+    }
+}
+
+
 window.addEventListener("DOMContentLoaded", () => {
 
     const audio = new Audio("drumsticks.wav");
@@ -12,9 +48,21 @@ window.addEventListener("DOMContentLoaded", () => {
     const playButton = document.querySelector("#play-button");
 
     playButton.addEventListener("click", () => {
-        setInterval(() => {
+
+        const ticker = new Ticking(30);
+
+        ticker.onTick(() => {
             playClick();
-        }, 1000);
+        });
+
+        ticker.onTick(() => {
+            console.count("tick!");
+        });
+
+        ticker.startPlaying();
+
     });
+
+
 });
 
