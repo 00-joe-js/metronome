adee39889cb407e383a9bf6fc7e73ae962913604
Joe Alves <jmalves@appacademy.io>
2021-04-18 15:36:18 -0400

    click every 60bpm

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("tick tick tick tick");
+window.addEventListener("DOMContentLoaded", () => {
+
+    const audio = new Audio("drumsticks.wav");
+    const playClick = () => {
+        audio.play();
+        setTimeout(() => {
+            audio.pause();
+            audio.currentTime = 0;
+        }, 800);
+    };
+
+    const playButton = document.querySelector("#play-button");
+
+    playButton.addEventListener("click", () => {
+        setInterval(() => {
+            playClick();
+        }, 1000);
+    });
+});
+
